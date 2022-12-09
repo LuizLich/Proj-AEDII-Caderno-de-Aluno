7f6077294919885a61fc3a51981c3802c25f4e42
Luiz Felipe TS <luiz.terrasilva22@gmail.com>
2022-12-09 15:31:16 -0300

    Fuctions Lembrete adicionadas

--- CadernoDeAluno.c
+++ CadernoDeAluno.c
@@ -13,7 +13,8 @@ typedef struct //Struct para armazenar os dados do flashcard
 } Flashcard; //Define o nome do novo tipo criado
 Flashcard lista[10];
 
-struct no { //Struct para percorrer a arvore
+struct no   //Struct para percorrer a arvore
+{
     struct no *esq;
     char dado[20];
     struct no *dir;
@@ -21,24 +22,48 @@ struct no { //Struct para percorrer a arvore
 typedef struct no *arvore;
 arvore T = NULL;
 
+
 typedef struct //Struct para armazenar os dados do quiz
 {
     char nome[50]; //Nome da materia
     int pontuacao, perguntas;
-}Materia; //Define o nome do novo tipo criado
+} Materia; //Define o nome do novo tipo criado
+
+
+struct Registro //Struct para os lembretes
+{
+    char texto[200];
+    struct Registro *prox;
+};
+
+typedef struct //Typedef referente ao Registro de lembretes
+{
+    struct Registro *inicio;
+    struct Registro *final;
+
+} Lista;
+
 Materia nomeM[10];
 
-void insereFlash();                     //Função que recebe os dados da materia a nota da mesma
-void pesquisaMateriaFlash();            //Função para receber o nome da matéria que o usuário deseja pesquisar
-void sortFlashcard(int tam);            //InsertionSort para ordenação dos dados no flashcard
-int pesquisaBinaria(char*, int, int);   //Função que faz a busca do dado desejado pelo usuário
-void funcQuiz();                        //Função para o sub-menu do Quiz para que o usuário digite os dados
-void insertNode(arvore *t, char d[20]); //Inserindo o no t para preparar a ordenação
+void insereFlash();                     //Fun��o que recebe os dados da materia a nota da mesma
+void pesquisaMateriaFlash();            //Fun��o para receber o nome da mat�ria que o usu�rio deseja pesquisar
+void sortFlashcard(int tam);            //InsertionSort para ordena��o dos dados no flashcard
+int pesquisaBinaria(char*, int, int);   //Fun��o que faz a busca do dado desejado pelo usu�rio
+void funcQuiz();                        //Fun��o para o sub-menu do Quiz para que o usu�rio digite os dados
+void insertNode(arvore *t, char d[20]); //Inserindo o no t para preparar a ordena��o
 void inOrdem(arvore t);                 //Mostrar as materias inseridas no quiz em ordem alfabetica
+void imprime_lista(Lista*lista2);       //Imprimir os lembretes
+void le(char *texto);                   //Receber a variável texto para incluir na lista
+int insere(Lista *lista2,char *texto);  //Incluir na lista o que texto recebeu
+void cria(Lista *lista2);               //Criar lista para lembretes
 
 int main()
 {
     int op = -1;
+    char texto[200] = {0};
+    struct Registro *elemento;
+    Lista lista2;
+    cria(&lista2);
 
     printf("\tBem-vindo(a)!\n\n ");
     do
@@ -50,10 +75,10 @@ int main()
 
         switch(menu)
         {
-        case 1:
+        case 1: //Menu do lembtrete, onde o aluno pode deixar anotações importantes
             printf("\n\n______ Lembretes ______\n\n");
 
-            printf ("Digite o que deseja (1 para ver,  2 para adicionar, 0 pra voltar): ");
+            printf ("\nDigite o que deseja (1 para ver,  2 para adicionar, 0 pra voltar): ");
             scanf ("%d", &menulembrete);
             getchar();
 
@@ -62,7 +87,7 @@ int main()
                 if(menulembrete == 1)
                 {
                     printf ("\nMostrando lembretes...\n\n");
-                    //chamar a fun��o de ver lembretes
+                    imprime_lista(&lista2);
                     printf ("1 para ver,  2 para adicionar, 0 pra voltar: ");
                     scanf ("%d", &menulembrete);
                 }
@@ -70,7 +95,10 @@ int main()
                 if(menulembrete == 2)
                 {
                     printf ("\nAdicione o lembrete\n\n");
-                    //Chamar a fun��o de adicionar lembrete
+                    le(texto);
+                    insere(&lista2, texto);
+                    getchar();
+                    memset(texto, '\0', 200);
                     printf ("1 para ver,  2 para adicionar, 0 pra voltar: ");
                     scanf ("%d", &menulembrete);
                 }
@@ -91,14 +119,14 @@ int main()
             }
             break;
 
-        case 2: //Menu do FlashCard, onde o aluno pode armazenar as notas totais de cada matéria, por exemplo: Modelagem 3D - 90; AOC - 70
+        case 2: //Menu do FlashCard, onde o aluno pode armazenar as notas totais de cada mat�ria, por exemplo: Modelagem 3D - 90; AOC - 70
             printf("\n\n______ FlashCards ______\n\n");
 
             printf ("Digite o que deseja (1 mostrar materias,  2 inserir materia, 3 pesquisar materia ou 0 para sair): ");
             scanf ("%d", &menuflashcard);
             getchar();
 
-            while(menuflashcard == 1 || 2 || 3) //Loop para que continue sendo executado enquanto o usuario não digitar 0 para voltar ao menu principal
+            while(menuflashcard == 1 || 2 || 3) //Loop para que continue sendo executado enquanto o usuario n�o digitar 0 para voltar ao menu principal
             {
                 if(menuflashcard == 1)
                 {
@@ -114,12 +142,12 @@ int main()
                 if(menuflashcard == 2)
                 {
                     if(indice < 10)
-                {
-                    printf("\nInserindo materia...\n");
-                    insereFlash();
-                }
+                    {
+                        printf("\nInserindo materia...\n");
+                        insereFlash();
+                    }
                     else
-                    printf("Maximo de materias!\n");
+                        printf("Maximo de materias!\n");
 
                     printf ("\n1 mostrar materias,  2 inserir materia, 3 pesquisar materia ou 0 para sair: ");
                     scanf ("%d", &menuflashcard);
@@ -159,7 +187,7 @@ int main()
             scanf ("%d", &menuquiz);
             getchar();
 
-            while(menuquiz != 0) //Loop para que continue sendo executado enquanto o usuario não digitar 0 para voltar ao menu principal
+            while(menuquiz != 0) //Loop para que continue sendo executado enquanto o usuario n�o digitar 0 para voltar ao menu principal
             {
                 if(menuquiz == 1)
                 {
@@ -170,12 +198,13 @@ int main()
                     getchar();
                 }
 
-                if(menuquiz == 2){
+                if(menuquiz == 2)
+                {
                     printf("\n\n------------------------------------------ Mostrando materias ------------------------------------------\n");
                     printf("Ordem alfabetica (A-Z):\n\n");
                     inOrdem(T);
                     printf("\n=====================================================================\n\n");
-                break;
+                    break;
                 }
 
                 if(menuquiz == 0)
@@ -207,7 +236,7 @@ int main()
     while (menu != 0);
 }
 
-void funcQuiz (){ //Onde o usuario informa sobre o quiz de autoavaliação
+void funcQuiz (){  //Onde o usuario informa sobre o quiz de autoavalia��o
     printf("Digite o nome da materia: ");
     scanf("%s", nomeM[numero].nome);
     getchar();
@@ -229,32 +258,35 @@ void funcQuiz (){ //Onde o usuario informa sobre o quiz de autoavaliação
     {
         printf("\nVoce precisa estudar mais na materia: %s! Abaixo da media\n", nomeM[numero].nome);
     }
-    insertNode(&T, nomeM[numero].nome); //Endereço T receberá o nome de cada matéria adicionada
+    insertNode(&T, nomeM[numero].nome); //Endere�o T receber� o nome de cada mat�ria adicionada
     numero++;
 }
 
-void insertNode(arvore *t, char d[20]){ //t é inserido na árvore
-    if (*t == NULL) {
-        *t = (struct no*) malloc(sizeof(struct no));//Alocando memória e verificando se tem espaço suficiente
-        if (*t != NULL) {
+void insertNode(arvore *t, char d[20]){  //t � inserido na �rvore
+    if (*t == NULL)
+    {
+        *t = (struct no*) malloc(sizeof(struct no));//Alocando mem�ria e verificando se tem espa�o suficiente
+        if (*t != NULL)
+        {
             (*t)->esq = NULL;
             (*t)->dir = NULL;
             strcpy((*t)->dado, d);
-        } else
-            printf("ERRO: Memoria insuficiente!");
-    } else
-        if (strcmp(d,(*t)->dado) < 0) //Comparando os dados e informando se há uma duplicação ou não
-            insertNode(&(*t)->esq, d);
+        }
         else
-            if (strcmp(d, (*t)->dado) > 0)
-                insertNode(&(*t)->dir, d);
-            else
-                printf("ATENCAO: Duplicacao de no!");
+            printf("ERRO: Memoria insuficiente!");
+    }
+    else if (strcmp(d,(*t)->dado) < 0)  //Comparando os dados e informando se h� uma duplica��o ou n�o
+        insertNode(&(*t)->esq, d);
+    else if (strcmp(d, (*t)->dado) > 0)
+        insertNode(&(*t)->dir, d);
+    else
+        printf("ATENCAO: Duplicacao de no!");
     return;
 }
 
-void inOrdem(arvore t){ //Função para organizar e printar em ordem ALFABETICA
-    if (t != NULL) {
+void inOrdem(arvore t){  //Fun��o para organizar e printar em ordem ALFABETICA
+    if (t != NULL)
+    {
         inOrdem(t->esq);
         printf("- %s\n", t->dado);
         inOrdem(t->dir);
@@ -262,7 +294,7 @@ void inOrdem(arvore t){ //Função para organizar e printar em ordem ALFABETICA
     return;
 }
 
-void insereFlash(){ //Função que recebe os dados da materia a nota da mesma e logo em seguida lança para o sort
+void insereFlash(){  //Fun��o que recebe os dados da materia a nota da mesma e logo em seguida lan�a para o sort
     printf("Digite o materia da materia: ");
     scanf("%s", &lista[indice].materia);
     getchar();
@@ -273,7 +305,7 @@ void insereFlash(){ //Função que recebe os dados da materia a nota da mesma e 
     sortFlashcard(indice);
 }
 
-void pesquisaMateriaFlash(){  //Função para receber o nome da matéria que o usuário deseja pesquisar e que é enviado para a pesquisa binaria
+void pesquisaMateriaFlash(){   //Fun��o para receber o nome da mat�ria que o usu�rio deseja pesquisar e que � enviado para a pesquisa binaria
     printf("Digite o nome da materia: ");
     scanf("%s", &pesquisa);
     getchar();
@@ -281,7 +313,7 @@ void pesquisaMateriaFlash(){  //Função para receber o nome da matéria que o u
     printf("Nota de %s: %s\n", pesquisa, lista[indiceUtilitario].nota);
 }
 
-void sortFlashcard(int tam){ //InsertionSort para ordenação dos dados no flashcard e jogando para a lista e seu aux
+void sortFlashcard(int tam){  //InsertionSort para ordena��o dos dados no flashcard e jogando para a lista e seu aux
     int k, j;
     Flashcard aux;
     for (k = 1; k <= tam - 1; k++)
@@ -298,8 +330,8 @@ void sortFlashcard(int tam){ //InsertionSort para ordenação dos dados no flash
     return;
 }
 
-int pesquisaBinaria(char dado[], int inicio, int fim){ //Função que faz a busca do dado desejado pelo usuário que é informado no menu do flashcard
- //É feito a comparação utilizando o método de pesquisa binária
+int pesquisaBinaria(char dado[], int inicio, int fim){  //Fun��o que faz a busca do dado desejado pelo usu�rio que � informado no menu do flashcard
+//� feito a compara��o utilizando o m�todo de pesquisa bin�ria
     int meio = (inicio + fim)/2;
     if (strcmp(lista[meio].materia, dado) == 0)
         return(meio);
@@ -310,4 +342,38 @@ int pesquisaBinaria(char dado[], int inicio, int fim){ //Função que faz a busc
     else
         pesquisaBinaria(dado, meio+1, fim);
     return 0;
+}
+
+void cria(Lista *lista2){ //Cria a lista para lembretes
+    lista2->inicio=NULL;
+    lista2->final=NULL;
+}
+
+int insere(Lista *lista2,char *texto){ //Nesta função utilizamos o malloc para alocação dinamica, e atribuimos texto a lista de lembretes
+    struct Registro *aux=(struct Registro*)malloc(sizeof(struct Registro));
+    if(aux == NULL) return 0;
+    strcpy(aux->texto,texto);
+    aux->prox=NULL;
+
+    if(lista2->inicio==NULL)
+        lista2->inicio=lista2->final=aux;
+    else
+    {
+        lista2->final->prox=aux;
+        lista2->final=aux;
+    }
+    return 1;
+}
+
+void le(char *texto){ //Recebe a variável texto
+printf("\nEscreva seu recadinho: ");
+scanf("%[^\n]s",texto);
+}
+
+void imprime_lista(Lista*lista2){ //Imprimir lista de lembretes
+struct Registro*aux = lista2->inicio;
+while(aux!=NULL){
+printf("\n%s\n", aux->texto);
+aux=aux->prox;
+}
 }
